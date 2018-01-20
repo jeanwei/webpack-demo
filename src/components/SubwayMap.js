7bae132150d45587f258fa7baffe506f823683b2
jeanwei <jeanwei938@msn.com>
2018-01-20 23:37:53 +0800

    Add station markers to map

--- src/components/SubwayMap.js
+++ src/components/SubwayMap.js
@@ -33,11 +33,9 @@ export default class SubwayMap extends LMap.Map {
     };
     this.markers = [];
     this.stationNames = [];
-
-    // this.markerBounds = this.addAllStationMarkers();
+    this.markerBounds = this.addAllStationMarkers();
 
     this.setCustomView();
-    console.log(this);
   }
 
   setCustomView() {
@@ -60,29 +58,32 @@ export default class SubwayMap extends LMap.Map {
 
   createCircleMarker(title, latlng, color, popup) {
     if (latlng) {
-      const marker = this.L.circleMarker(latlng,
-        {
-          title: title,
+      const marker = this.L.circleMarker(
+        latlng,
+        { title: title,
           riseOnHover: true,
           radius: 5,
           stroke: false,
           color: color,
-          fillOpacity: 0.6,
-        });
+          fillOpacity: 0.6 }
+      );
 
-      marker.bindLabel(title, {
-        noHide: false,
-        clickable: true,
-        direction: 'auto',
-        className: 'station-name'
-      })
+      marker.bindTooltip(
+        title,
+        { permanent: false,
+          interactive: true,
+          direction: 'auto',
+          className: 'station-name' }
+      );
+      marker.bindPopup(popup);
+      return marker;
     }
   }
-  createStationMarker(unit, s) {
-    if(s.coords && s.coords.lat && s.coors.lng) {
-      const latlng = [s.coords.lat, s.coors.lng];
-      const lines = this.lineToIcons(s.line_name);
 
+  createStationMarker(unit, s) {
+    if(s.coords && s.coords.lat && s.coords.lng) {
+      const latlng = [s.coords.lat, s.coords.lng];
+      const lines = this.lineToIcons(s.line_name);
       const popupContent = `<div class='map-popup'><span class='title'>${s.station_name} <small class='unit'>${unit}</small></span>${lines}</div>`;
       const popup = this.createPopUp(latlng, popupContent);
 
@@ -94,31 +95,46 @@ export default class SubwayMap extends LMap.Map {
     }
   }
 
+  // for each layer in this
+  eachLineLayer(f) {
+    const lines = this.layers['lines'];
+    for (let linename in lines) {
+      const layer = lines[linename];
+      f(linename, layer);
+    }
+  }
+
+  // add stations and returns their bounds
   addAllStationMarkers() {
     const stations = this.stations;
-
     const markers = [];
-    const lls = [];
+    const latLngs = [];
     for (let unit in stations) {
       const data = stations[unit];
-      const m = this.createStationMarker(unit, data);
-      if (m) {
-        markers.push(m);
-        lls.push(m.getLatLng());
+      const marker = this.createStationMarker(unit, data);
+      if (marker) {
+        markers.push(marker);
+        latLngs.push(marker.getLatLng());
       }
     }
 
-    markers.forEach(m => {
-      if (m == null) return;
+    // for each marker, add to layer for their line
+    markers.forEach(marker => {
+      if (marker == null) return;
 
-      const lines = m.lineName.split("");
+      const lines = marker.lineName.split("");
       lines.forEach(l => {
+        console.log('initial layer lines', this.layers['lines']);
         if (l in this.layers['lines']) {
-          this.layers['lines'][l].addLayer(m);
+          this.layers['lines'][l].addLayer(marker);
         } else {
-          this.layers['lines'][l] = this.L.featureGroup([m]);
+          this.layers['lines'][l] = this.L.featureGroup([marker]);
         }
+        console.log('updated layers', this.layers['lines']);
       })
     })
+
+    this.eachLineLayer((linename, layer) => layer.addTo(this));
+    return this.L.latLngBounds(latLngs);
   }
 }
